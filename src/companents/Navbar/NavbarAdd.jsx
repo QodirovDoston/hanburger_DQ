a60ac687b5d94e3c5d1ff179dce0616539f393a6
QodirovDoston <qodirovdoston806@gmail.com>
2023-03-22 18:07:56 +0500

    dfdfdf

--- src/companents/Navbar/NavbarAdd.jsx
+++ src/companents/Navbar/NavbarAdd.jsx
@@ -4,13 +4,9 @@ import { Link } from 'react-router-dom'
 import LunchDiningIcon from '@mui/icons-material/LunchDining';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
-
-
 import logo from '../img/logo.png'
 
 const NavbarAdd = () => {
-
-
 
   const [open, setOpen] = React.useState(false);
   const handleClose = () => {
@@ -27,7 +23,6 @@ const NavbarAdd = () => {
           <Link to={"https://www.youtube.com/watch?v=IPnmNq5BEzo"}>
             <img style={{marginTop:'-10px', borderRadius:'50%', border:'none' ,width:'114px', height:'80px'}} src={logo} alt="" />
           </Link>
-
           <Link to='/table' style={{ color: 'black', textDecoration: 'none', paddingRight: '20px' }}>
             <li onClick={handleToggle} style={{ marginTop: '9px' }}>Lavash</li>
           </Link>
@@ -65,13 +60,11 @@ const NavbarAdd = () => {
             <CircularProgress color="inherit" />
           </Backdrop>
 
-        
           <LunchDiningIcon
             style={{ marginTop: '10px', marginLeft: '650px', fontSize: "50px" }}
             color='warning'
-            sx={{ flexGrow: 1 }}
+            sx={{ flexGrow: +1 }}
             />
-            {/* </Row> */}
         </Nav>
       </Container>
 
